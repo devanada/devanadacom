4ed2617e4835fa593532203509932094a95b6feb
Yoga S Devanada <yogasdevanada@gmail.com>
2025-07-28 16:19:46 +0700

    feat: add context menu for showing right click menu

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,12 @@
 import { type ReactNode } from "react";
+
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { Separator } from "@/components/ui/separator";
 import Taskbar from "./taskbar";
 
 interface LayoutProps {
@@ -9,7 +17,25 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="w-full grow flex flex-col bg-[url(https://i.imgur.com/Zk6TR5k.jpeg)] bg-cover overflow-hidden bg-no-repeat bg-center">
-        {children}
+        <ContextMenu>
+          <ContextMenuTrigger className="w-full h-full">
+            {children}
+          </ContextMenuTrigger>
+          <ContextMenuContent className="rounded-none w-64">
+            <ContextMenuItem>Refresh</ContextMenuItem>
+            <Separator />
+            <ContextMenuItem disabled aria-disabled>
+              Paste
+            </ContextMenuItem>
+            <ContextMenuItem disabled aria-disabled>
+              Paste Shortcut
+            </ContextMenuItem>
+            <Separator />
+            <ContextMenuItem>New</ContextMenuItem>
+            <Separator />
+            <ContextMenuItem>Properties</ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
       </div>
       <Taskbar />
     </div>
